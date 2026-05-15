6b0c5be779e4c70293990623d6a0170166bbc813
Sun <rlatjsgml128@gmail.com>
2026-05-15 15:06:22 +0900

    [NEW] youtube 및 aistudio api

---
+++ 7.PYTHON/11.API/7.youtube_detail_save.py
@@ -0,0 +1,53 @@
+# pip install python-dotenv
+
+import csv
+import os
+import requests
+from dotenv import load_dotenv
+
+# .env 로드
+load_dotenv()
+
+API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+# API URL
+video_api_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+video_ids = []
+
+with open('search_result.csv', 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        video_ids.append(row['video_id'])
+
+# print(video_ids)
+
+params = {
+    'part': 'snippet,statistics',
+    'id':','.join(video_ids),
+    'key': API_KEY
+}
+
+# 최종 결과 저장용
+table = []
+
+# ⭐ API 호출 (이게 핵심)
+response = requests.get(video_api_url, params=params)
+data = response.json()
+
+# 테이블 헤더
+table_header = ['video_id', 'title', 'view_count', 'like_count', 'comment_count']
+
+with open('video_stats.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(table_header)
+
+    for item in data.get('items', []):
+        video_id = item['id']
+        title = item['snippet']['title']
+        stats = item['statistics']
+        view_count = stats.get('viewCount',0)
+        like_count = stats.get('likeCount',0)
+        comment_count = stats.get('commentCount',0)
+
+        writer.writerow([video_id, title, view_count, like_count, comment_count])
---
+++ 7.PYTHON/11.API/9.aistudio_new.py
@@ -0,0 +1,15 @@
+# pip install google-genai
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv('GEMINI-API_KEY'))
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents='나는 한국에 살고 있는 20대 개발자야, 오늘 저녁에 뭐 먹으면 좋은지 알려줘'
+)
+
+print(response.text)
